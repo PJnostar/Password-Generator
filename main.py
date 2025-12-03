14b8864a50655b227bb5bfe263a4a3bf7642c7e8
PJnostar <piotr.jurkowskii@gmail.com>
2025-12-03 12:29:22 +0100

    Added function to generate passwords with constraints.

--- main.py
+++ main.py
@@ -10,12 +10,23 @@ alphabet = letters + digits + special_chars
 pwd_length = 12
 
 #function to generate password
-def generate_password(length=pwd_length):
+def generatePassword(length=pwd_length):
     pwd = ''
     for i in range(length):
         pwd += ''.join(secrets.choice(alphabet)) 
     return pwd
 
+def generatePasswordConstraints():
+    while True:
+        pwd = ''
+        for i in range(pwd_length):
+            pwd += ''.join(secrets.choice(alphabet))
+        if ( any(special_char in special_chars for special_char in pwd) and
+            sum(char in digits for char in pwd) >= 2):
+            break
+    return pwd
+
 #main function
 if __name__ == "__main__":
-    print("Generated Password: ", generate_password())
+    print("Generated Password: ", generatePassword())
+    print("Generated Password with Constraints: ", generatePasswordConstraints())
